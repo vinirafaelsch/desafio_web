5b9c509c42d154d2c41af499dbb0faeb299dd2b8
vinirafaelsch <vinirafaelsch@gmail.com>
2021-01-22 09:07:27 -0300

    final

--- web/src/views/PessoasCadastradas.jsx
+++ web/src/views/PessoasCadastradas.jsx
@@ -27,51 +27,30 @@ class PessoasCadastradas extends React.Component {
                     <br/>
                     <h1 className="font-weight-light text-center">Pessoas cadastradas</h1>
                     <hr/>
-                    <div className="row featurette">
-                        <div className="col-md-7">
-                            <h2 className="featurette-heading">asdfads<span className="text-muted">It'll blow your mind.</span>
-                            </h2>
-                            <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula
-                                porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                                Fusce dapibus, tellus ac cursus commodo.</p>
+                    {/*Printa Pessoas Cadastradas*/}
+                    {this.state.users.map((user) => (
+                        <div className="row featurette" style={{marginTop: "30px"}}>
+                            <div className="col-md-7">
+                                <h2 className="featurette-heading">{ user.name }<span className="text-muted"> Rua: { user.address.street }, { user.address.suite }</span>
+                                </h2>
+                                <p className="lead">
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                                    <br /><br />
+                                    <strong>Telefone: { user.phone } </strong> <br />
+                                    <strong>Email: { user.email } </strong>
+                                </p>
+                            </div>
+                            <div className="col-md-5">
+                                <img className="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto"
+                                     alt="500x500" style={{width: "500px"}}
+                                     src="https://imagens3.ne10.uol.com.br/blogsne10/social1/uploads/2020/09/mariahdemoraes.jpg"
+                                     data-holder-rendered="true" />
+                            </div>
                         </div>
-                        <div className="col-md-5">
-                            <img className="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto"
-                                 alt="500x500" style={{width: "500px"}}
-                                 src="https://imagens3.ne10.uol.com.br/blogsne10/social1/uploads/2020/09/mariahdemoraes.jpg"
-                                 data-holder-rendered="true" />
-                        </div>
-                    </div>
-                    <div className="row featurette">
-                        <div className="col-md-7 order-md-2">
-                            <h2 className="featurette-heading">Oh yeah, it's that good. <span className="text-muted">See for yourself.</span>
-                            </h2>
-                            <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula
-                                porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl
-                                consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-                        </div>
-                        <div className="col-md-5 order-md-1">
-                            <img className="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto"
-                                 alt="500x500"
-                                 src="https://ibac.com.br/wp-content/uploads/2017/05/Andr%C3%A9-Farias-Pessoa-500x500.jpg"
-                                 data-holder-rendered="true" style={{width: "500px"}} />
-                        </div>
-                    </div>
-                    <div className="row featurette">
-                        <div className="col-md-7">
-                            <h2 className="featurette-heading">First featurette heading. <span className="text-muted">It'll blow your mind.</span>
-                            </h2>
-                            <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula
-                                porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                                Fusce dapibus, tellus ac cursus commodo.</p>
-                        </div>
-                        <div className="col-md-5">
-                            <img className="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto"
-                                 alt="500x500" style={{width: "500px"}}
-                                 src="https://marcuspessoa.com.br/wp-content/uploads/2019/06/Marcus-Pessoa-Cidades-Criativas-1.jpg"
-                                 data-holder-rendered="true" />
-                        </div>
-                    </div>
+                    ))}
                 </div>
                 <Footer />
             </div>
